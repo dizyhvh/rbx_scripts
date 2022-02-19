d8a7c0cce959ef6f3a5f96523cf3eb70acf1cc18
dizyhvh <80992179+dizyhvh@users.noreply.github.com>
2022-02-19 20:55:47 +0300

    fixed auto stream, refill energy and auto sell followers (devs have no life, they patched it so i had to recode it a lil bit)

--- streaming_simulator.lua
+++ streaming_simulator.lua
@@ -5,6 +5,7 @@ local w = library:CreateWindow("by dizy#5334")
 
 local tab1 = w:CreateFolder("Farming")
 
+local dont_enable_stream = false;
 getgenv().AutoStream = false;
 getgenv().RefillEnergy = false;
 getgenv().AutoSellFollowers = false;
@@ -35,11 +36,15 @@ spawn(function()
         wait()
         if getgenv().AutoStream then
             if game:GetService("Players").LocalPlayer:FindFirstChild("values"):FindFirstChild("Streaming").Value == false then
-                game:GetService("ReplicatedStorage").StreamChange:FireServer("On")
+                if not dont_enable_stream then
+                    game:GetService("ReplicatedStorage").StreamChange:FireServer("On");
+                end
             elseif game:GetService("Players").LocalPlayer:FindFirstChild("values"):FindFirstChild("Streaming").Value == true then
-                if getgenv().RefillEnergy then if game:GetService("Players").LocalPlayer:FindFirstChild("values"):FindFirstChild("Energy").Value <= (game:GetService("Players").LocalPlayer:FindFirstChild("values"):FindFirstChild("MaxEnergy").Value/3) then game:GetService("ReplicatedStorage").RewardEnergy:FireServer(game:GetService("Players").LocalPlayer:FindFirstChild("values"):FindFirstChild("MaxEnergy").Value); end end
-                if getgenv().AutoSellFollowers then firetouchinterest(game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart"), game:GetService("Workspace"):FindFirstChild("MedievalSell"):FindFirstChild("SellHB"), 0); firetouchinterest(game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart"), game:GetService("Workspace"):FindFirstChild("MedievalSell"):FindFirstChild("SellHB"), 1); end
+                if getgenv().RefillEnergy then if game:GetService("Players").LocalPlayer:FindFirstChild("values"):FindFirstChild("Energy").Value <= (game:GetService("Players").LocalPlayer:FindFirstChild("values"):FindFirstChild("MaxEnergy").Value/5) then dont_enable_stream = true; game:GetService("ReplicatedStorage").StreamChange:FireServer("Off"); game:GetService("ReplicatedStorage").RewardEnergy:FireServer(game:GetService("Players").LocalPlayer:FindFirstChild("values"):FindFirstChild("MaxEnergy").Value); game:GetService("ReplicatedStorage").StreamChange:FireServer("On"); dont_enable_stream = false; end end
+                if getgenv().AutoSellFollowers then game:GetService("ReplicatedStorage").StreamChange:FireServer("Off"); firetouchinterest(game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart"), game:GetService("Workspace"):FindFirstChild("MedievalSell"):FindFirstChild("SellHB"), 0); firetouchinterest(game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart"), game:GetService("Workspace"):FindFirstChild("MedievalSell"):FindFirstChild("SellHB"), 1); game:GetService("ReplicatedStorage").StreamChange:FireServer("On"); end
+                if game:GetService("Players").LocalPlayer:FindFirstChild("values"):FindFirstChild("ButtonTimer").Value ~= 0 then game:GetService("Players").LocalPlayer:FindFirstChild("values"):FindFirstChild("ButtonTimer").Value = 0; end
                 game:GetService("ReplicatedStorage").BubbleReward:FireServer();
+                wait(0.5);
                 game:GetService("ReplicatedStorage").EReward:FireServer();
             end
         end
