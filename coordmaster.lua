cbd3ee9e71fd325210ff65f9f26ae01f73195982
dizyhvh <80992179+dizyhvh@users.noreply.github.com>
2022-04-05 21:07:50 +0300

    Update coordmaster.lua

--- coordmaster.lua
+++ coordmaster.lua
@@ -37,7 +37,7 @@ function coordmaster:Teleport(position, step_length, step_delay, bypass_anti_tp,
                         game:GetService("Players").LocalPlayer.Character:FindFirstChildOfClass("Humanoid"):SetStateEnabled(Enum.HumanoidStateType.Freefall, false);
                         game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart").Anchored = false;
                         game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart").CFrame = CFrame.new(path[i].x, path[i].y, path[i].z);
-                        task.wait(0.01);
+                        task.wait(0.05);
                         game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart").Anchored = true;
                     else
                         game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart").CFrame = CFrame.new(path[i].x, path[i].y, path[i].z);
