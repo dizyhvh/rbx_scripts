bc5cd52d0e7d239ac29a4312205c252dc40624fc
dizyhvh <80992179+dizyhvh@users.noreply.github.com>
2022-06-17 01:31:06 +0300

    Update ability_wars.lua

--- ability_wars.lua
+++ ability_wars.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/dizyhvh/rbx_scripts/main/dizzy_hub/scripts/slap_battles.lua')))();
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/dizyhvh/rbx_scripts/main/dizzy_hub/scripts/ability_wars.lua')))();
