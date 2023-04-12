8af4b90dfea6b624798ca08e4f27e87409716941
dizyhvh <80992179+dizyhvh@users.noreply.github.com>
2023-04-12 17:38:24 +0300

    recoded script, added new velocity fix

--- libraries/coordmaster.lua
+++ libraries/coordmaster.lua
@@ -12,18 +12,33 @@ local coordmaster = {};
 local debounce = false;
 local debounce2 = {};
 
-function coordmaster:Teleport(args, callback)
-    assert(args ~= nil or typeof(args) ~= "table", "[Coordmaster] Arguments are nil/undefined. (should be table)");
-    assert(args["Position"] ~= nil, "[Coordmaster] Position is nil/undefined.");
-    assert(args["StepLength"] ~= nil, "[Coordmaster] Step length is nil/undefined.");
-    assert(args["StepDelay"] ~= nil, "[Coordmaster] Delay is nil/undefined.");
+--[[
+    Usage:
+     - coordmaster:Teleport({args["Position"] = CFrame.new(), args["Rotation"] = Vector3.new(), args["StepLength"] = 1, args["StepDelay"] = 0.1, args["StepType"] = 1, args["VelocityFix"] = 1}, callback);
+                           (destination position,            (Angles of Character)                                                              (1 - DEFAULT step,     (1 - UNSAFE fix,
+                           COULD BE Vector3.new())                                                                                               2 - DYNAMIC step)      2 - SAFE fix)
+]]
 
+function coordmaster:Teleport(args, callback)
     if debounce then
         return;
     end
     
-    if not (typeof(args["Rotation"]) == "CFrame" or typeof(args["Rotation"]) == "Vector3") or args["Rotation"] == nil then
+    assert(args ~= nil or typeof(args) ~= "table", "[Coordmaster] Arguments are nil/undefined. (should be table)");
+    assert(args["Position"] ~= nil, "[Coordmaster] Position is nil/undefined.");
+    assert(args["StepLength"] ~= nil, "[Coordmaster] Step length is nil/undefined.");
+    assert(args["StepDelay"] ~= nil, "[Coordmaster] Delay is nil/undefined.");
+    
+    if args["Rotation"] == nil or typeof(args["Rotation"]) ~= "CFrame" then
         args["Rotation"] = CFrame.Angles(0, math.rad(90), 0);
+    end
+    
+    if args["StepType"] == nil or typeof(args["StepType"]) ~= "number" then
+        args["StepType"] = 1; 
+    end
+    
+    if args["VelocityFix"] == nil or typeof(args["VelocityFix"]) ~= "number" then
+        args["VelocityFix"] = 1; 
     end
     
     if typeof(args["Position"]) == "CFrame" or typeof(args["Position"]) == "Vector3" then
@@ -54,32 +69,46 @@ function coordmaster:Teleport(args, callback)
             local stop_tping = false;
             if game:GetService("Players").LocalPlayer.Character ~= nil and game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart") ~= nil and (game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart").Position-Vector3.new(path[#path].x, path[#path].y, path[#path].z)).Magnitude <= 3.5 then
                 stop_tping = true;
-            end
-                
-            for i=1, steps do
-                if stop_tping then
-                    continue;
-                end
-                
-                if i > 1 and (game:GetService("Players").LocalPlayer.Character == nil or game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart") == nil) then
-                    debounce = false;
-                    return warn("[Coordmaster] Character's RootPart (HumanoidRootPart) got destroyed! For security reasons, script has previously stopped.");
-                end
-                
-                game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart").Velocity = Vector3.new(0, Random.new(tick()):NextInteger(17, 20), 0);
-                
-                if args["BypassAntiTP"] then
-                    game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart").Anchored = false;
+            elseif step["VelocityFix"] == 2 then
+                vel_fix = game:GetService("RunService").Stepped:Connect(function()
+                    if game:GetService("Players").LocalPlayer.Character == nil or game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart") == nil then
+                        vel_fix:Disconnect();
+                    end
                     
-                    game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart").CFrame = CFrame.new(path[i].x, path[i].y, path[i].z) * args["Rotation"];
-                    game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart").Anchored = true;
-                else
-                    game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart").CFrame = CFrame.new(path[i].x, path[i].y, path[i].z) * args["Rotation"];
+                    game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart").AssemblyAngularVelocity = Vector3.new(0, 0, 0);
+                    game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart").AssemblyLinearVelocity = Vector3.new(0, 0, 0);
+                    game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart").Velocity = Vector3.new(0, 0, 0);
+                end)
+            end
+            
+            if not stop_tping then
+                for i=1, steps do
+                    if i > 1 and (game:GetService("Players").LocalPlayer.Character == nil or game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart") == nil) then
+                        debounce = false;
+                        return warn("[Coordmaster] Character's RootPart (HumanoidRootPart) got destroyed! For security reasons, script has previously stopped.");
+                    end
+                    
+                    if args["VelocityFix"] <= 1 then
+                        game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart").Velocity = Vector3.new(0, Random.new(tick()):NextInteger(17, 20), 0);
+                    end
+                    
+                    if args["BypassAntiTP"] then
+                        game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart").Anchored = false;
+                        
+                        game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart").CFrame = CFrame.new(path[i].x, path[i].y, path[i].z) * args["Rotation"];
+                        game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart").Anchored = true;
+                    else
+                        game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart").CFrame = CFrame.new(path[i].x, path[i].y, path[i].z) * args["Rotation"];
+                    end
+                    
+                    task.wait(args["StepDelay"]);
                 end
-                
-                task.wait(args["StepDelay"]);
             end
 
+            if vel_fix ~= nil then
+                vel_fix:Disconnect();
+            end
+            
             if game:GetService("Players").LocalPlayer.Character ~= nil then
                 if game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart") ~= nil then
                     game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart").Anchored = false;
@@ -88,29 +117,45 @@ function coordmaster:Teleport(args, callback)
                     game:GetService("Players").LocalPlayer.Character:FindFirstChildOfClass("Humanoid"):SetStateEnabled(Enum.HumanoidStateType.Seated, true);
                 end
             end
-                
-            if callback ~= nil then
-                callback();
-            end
-
+            
             debounce = false;
+                
+            if callback ~= nil and typeof(callback) == "function" then
+                task.spawn(callback);
+            end
         end
     end
 end
 
+--[[
+    Usage:
+     - coordmaster:TeleportInstance({args["Instance"} = <BasePart>, args["Position"] = CFrame.new(), args["Rotation"] = Vector3.new(), args["StepLength"] = 1, args["StepDelay"] = 0.1, args["StepType"] = 1, args["VelocityFix"] = 1}, callback);
+                                                                  (destination position,            (Angles of Character)                                                              (1 - DEFAULT step,     (1 - UNSAFE fix,
+                                                                  COULD BE Vector3.new())                                                                                               2 - DYNAMIC step)      2 - SAFE fix)
+]]
+
 function coordmaster:TeleportInstance(args, callback)
     assert(args ~= nil or typeof(args) ~= "table", "[Coordmaster] Arguments are nil/undefined. (should be table)");
     assert(args["Instance"] ~= nil, "[Coordmaster] Instance is nil/undefined.");
-    assert(args["Position"] ~= nil, "[Coordmaster] Position is nil/undefined.");
-    assert(args["StepLength"] ~= nil, "[Coordmaster] Step length is nil/undefined.");
-    assert(args["StepDelay"] ~= nil, "[Coordmaster] Delay is nil/undefined.");
-
+    
     if table.find(debounce2, args["Instance"]) then
         return;
     end
     
-    if not (typeof(args["Rotation"]) == "CFrame" or typeof(args["Rotation"]) == "Vector3") or args["Rotation"] == nil then
+    assert(args["Position"] ~= nil, "[Coordmaster] Position is nil/undefined.");
+    assert(args["StepLength"] ~= nil, "[Coordmaster] Step length is nil/undefined.");
+    assert(args["StepDelay"] ~= nil, "[Coordmaster] Delay is nil/undefined.");
+    
+    if args["Rotation"] == nil or typeof(args["Rotation"]) ~= "CFrame" then
         args["Rotation"] = CFrame.Angles(0, math.rad(90), 0);
+    end
+    
+    if args["StepType"] == nil or typeof(args["StepType"]) ~= "number" then
+        args["StepType"] = 1; 
+    end
+    
+    if args["VelocityFix"] == nil or typeof(args["VelocityFix"]) ~= "number" then
+        args["VelocityFix"] = 1; 
     end
     
     if typeof(args["Position"]) == "CFrame" or typeof(args["Position"]) == "Vector3" then
@@ -146,44 +191,60 @@ function coordmaster:TeleportInstance(args, callback)
             end
             path[#path+1] = {x = args["Position"].X, y = args["Position"].Y, z = args["Position"].Z};
             
+            local vel_fix = nil;
             local stop_tping = false;
             if args["Instance"] ~= nil and (args["Instance"].Position-Vector3.new(path[#path].x, path[#path].y, path[#path].z)).Magnitude <= 3.5 then
-                return;
+                stop_tping = true;
+            elseif step["VelocityFix"] == 2 then
+                vel_fix = game:GetService("RunService").Stepped:Connect(function()
+                    if args["Instance"] == nil then
+                        vel_fix:Disconnect();
+                    end
+                    
+                    game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart").AssemblyAngularVelocity = Vector3.new(0, 0, 0);
+                    game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart").AssemblyLinearVelocity = Vector3.new(0, 0, 0);
+                    game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart").Velocity = Vector3.new(0, 0, 0);
+                end)
             end
             
-            for i=1, steps do
-                if stop_tping then
-                    continue;
-                end
-                
-                task.wait(args["StepDelay"]);
-                
-                if i > 1 and args["Instance"] == nil then
-                    debounce = false;
-                    return warn("[Coordmaster] Instance got destroyed! For security reasons, script has previously stopped.");
-                end
-                
-                args["Instance"].Velocity = Vector3.new(0, Random.new(tick()):NextInteger(17, 20), 0);
-                if args["BypassAntiTP"] then
-                    args["Instance"].Anchored = false;
-
-                    args["Instance"].CFrame = CFrame.new(path[i].x, path[i].y, path[i].z) * args["Rotation"];
-                    args["Instance"].Anchored = true;
-                else
-                    args["Instance"].CFrame = CFrame.new(path[i].x, path[i].y, path[i].z) * args["Rotation"];
+            if not stop_tping then
+                for i=1, steps do
+                    if i > 1 and args["Instance"] == nil then
+                        debounce = false;
+                        return warn("[Coordmaster] Instance got destroyed! For security reasons, script has previously stopped.");
+                    end
+                    
+                    if step["VelocityFix"] <= 1 then
+                        args["Instance"].Velocity = Vector3.new(0, Random.new(tick()):NextInteger(17, 20), 0);
+                    end
+                    
+                    if args["BypassAntiTP"] then
+                        args["Instance"].Anchored = false;
+    
+                        args["Instance"].CFrame = CFrame.new(path[i].x, path[i].y, path[i].z) * args["Rotation"];
+                        args["Instance"].Anchored = true;
+                    else
+                        args["Instance"].CFrame = CFrame.new(path[i].x, path[i].y, path[i].z) * args["Rotation"];
+                    end
+                    
+                    task.wait(args["StepDelay"]);
                 end
             end
 
+            if vel_fix ~= nil then
+                vel_fix:Disconnect();
+            end
+            
             if args["Instance"] ~= nil then
                 args["Instance"].Anchored = false;
+                
+                if table.find(debounce2, args["Instance"]) then
+                    table.remove(debounce2, table.find(debounce2, args["Instance"]));
+                end
             end
                
-            if callback ~= nil then
-                callback();
-            end
-
-            if table.find(debounce2, args["Instance"]) then
-                table.remove(debounce2, table.find(debounce2, args["Instance"]));
+            if callback ~= nil and typeof(callback) == "function" then
+                task.spawn(callback);
             end
         end
     end
